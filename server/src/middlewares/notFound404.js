15685972b93d92a93a1d74f7313bdbae4903ca9e
Javier Scavuzzo <javierscavuzzo17@gmail.com>
2025-02-12 21:03:43 +0100

    error 404 prueba

---
+++ server/src/middlewares/notFound404.js
@@ -0,0 +1,8 @@
+const notFoundMiddleware = (req, res) => {
+    res.status(404).json({
+        status: 'error',
+        message: 'Ruta no encontrada',
+    });
+};
+
+module.exports = notFoundMiddleware;
