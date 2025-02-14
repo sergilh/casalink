be3f30874a8d5f7e7111c8b9a445f7dc09c496b8
Ken Russel Claveria <koiclaveria@gmail.com>
2025-02-15 00:48:39 +0100

    Guardando progreso del endpoint info Usuario

--- server/src/routes/user/usersRoutes.js
+++ server/src/routes/user/usersRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import usersController from '../../controllers/user/usersController.js';
+import authUserMiddleware from '../../middlewares/authUserMiddleware.js';
+import getUserInfo from '../../controllers/user/usersInfoController.js';
 
 const router = express.Router();
 
 // Endpoint para obtener la lista de usuarios
 router.get('/users', usersController);
+
+//Endpoint protegido para obtener la información del usuario autenticado
+router.get('/profile', authUserMiddleware, getUserInfo);
 
 export default router;
