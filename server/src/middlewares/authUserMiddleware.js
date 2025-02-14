87e441b5fcaa01aeaad606088404b0fe5077b7fc
Ken Russel Claveria <koiclaveria@gmail.com>
2025-02-14 11:44:19 +0100

    Guardando progreso del middleware JWT/ creado función que conecta al bbdd (models)

--- server/src/middlewares/authUserMiddleware.js
+++ server/src/middlewares/authUserMiddleware.js
@@ -5,15 +5,16 @@ import generateErrorUtil from '../utils/generateErrorUtil.js';
 // Inicia el middleware.
 const authUserMiddleware = async (req, res, next) => {
     try {
+        //accede a los headers
         const { authorization } = req.headers;
-
+        //si no hay header lanza error
         if (!authorization) {
             generateErrorUtil('Falta la cabecera de autorización', 401);
         }
-
+        //si hay header lo desencripta
         try {
             const tokenInfo = jwt.verify(authorization, process.env.SECRET);
-
+            //agregar el token al objero user en el objero req
             req.user = tokenInfo;
 
             next();
---
+++ server/src/models/users/selectUserByIdModel.js
@@ -0,0 +1,33 @@
+import getPool from '../../db/pool.js';
+
+import generateErrorUtil from '../../utils/generateErrorUtil.js';
+
+// Inicializamos el modelo.
+const selectUserByIdModel = async (userId) => {
+    const pool = await getPool();
+
+    const [users] = await pool.query(
+        `
+            SELECT
+                id,
+                username,
+                firstName,
+                lastName,
+                birthDate,
+                email,
+                avatar,
+                role,
+                createdAt
+            FROM users WHERE id = ?
+        `,
+        [userId]
+    );
+
+    if (users.length < 1) {
+        generateErrorUtil('Usuario no encontrado', 404);
+    }
+
+    return users[0];
+};
+
+export default selectUserByIdModel;
