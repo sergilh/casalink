efc3b22574f947a919af09c876af617b093b753c
Ken Russel Claveria <koiclaveria@gmail.com>
2025-02-18 10:44:02 +0100

    Actualizar y crear requestvisit controller y router

---
+++ server/src/routes/user/requestVisitRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import requestVisitController from '../../controllers/user/requestVisitController.js';
+
+import authUserMiddleware from '../../middlewares/authUserMiddleware.js';
+
+const router = express.Router();
+
+// Endpoint para solicitar una visita( requiere autenticación)
+
+router.post('/request-visit', authUserMiddleware, requestVisitController);
+
+export default router;
