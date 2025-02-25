4025d7182ab717031e0773e8cb3ea7c7648ed78d
johnatanmoran <info@johnatanmoran.com>
2025-02-25 12:16:46 +0100

    AJustes y puesta a punto de últimos detalles del backend

--- server/src/controllers/users/usersModificationController.js
+++ server/src/controllers/users/usersModificationController.js
@@ -5,6 +5,20 @@ const usersModificationController = async (req, res, next) => {
 	const { id } = req.params;
 	const { name, lastName, legalId, email, phone, bio } = req.body;
 
+	/*
+	id                     INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name                   VARCHAR(100),
+	lastName               VARCHAR(100),
+	legalId                VARCHAR(20) UNIQUE,
+	email                  VARCHAR(100) UNIQUE NOT NULL,
+	password               VARCHAR(255), -- hashed
+	phone                  VARCHAR(255), -- hashed
+	avatarUrl              VARCHAR(255),
+	bio                    TEXT,
+
+	updatedAt              TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+*/
+
 	const pool = await getPool();
 
 	const query = `UPDATE users SET name = ?, lastName = ?, legalId = ?, email = ?, phone = ?, bio = ? WHERE id = ?;`;
