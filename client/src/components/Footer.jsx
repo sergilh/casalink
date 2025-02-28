563f907d0999ec3e845db72dbbffc55ecfc8ba89
johnatanmoran <info@johnatanmoran.com>
2025-02-28 23:46:10 +0100

    Branch de landing page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
 	return (
-		<footer>
-			<p>&copy; Casalink app 2025</p>
+		<footer className="bg-cyan-200 py-8">
+			<div className="container mx-auto px-4 flex justify-end">
+				<div className="text-2xl font-bold text-cyan-600">
+					<span>casa</span>
+					<span>link</span>
+					<p>&copy; Casalink app 2025</p>
+				</div>
+			</div>
 		</footer>
 	);
 };
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,39 @@
+import casalinkLogo from '../assets/images/casalink-logotipo_main_1080x400.svg';
 import { Link } from 'react-router-dom';
+
 const Header = () => {
 	return (
-		<header>
-			<h1>
-				<Link to="/">Casalink</Link>
-			</h1>
-
-			<nav>
-				<ul>
-					<li>
-						<Link to="/login">Login</Link>
-					</li>
-					<li>
-						<Link to="/register">Register</Link>
-					</li>
-				</ul>
+		<header className="bg-gray-100 py-4 px-6 flex items-center justify-between">
+			<div className="flex items-center">
+				<div className="text-2xl font-bold">
+					<Link to="/">
+						<img
+							src={casalinkLogo}
+							className="logo"
+							alt="Casalink"
+						/>
+					</Link>
+				</div>
+			</div>
+			<nav className="hidden md:flex items-center space-x-8">
+				<a href="#" className="text-gray-600">
+					Acerca
+				</a>
+				<a href="#" className="text-gray-600">
+					Contacto
+				</a>
+				<a href="#" className="text-gray-600">
+					Ayuda
+				</a>
+				<a href="#" className="text-gray-600 font-medium">
+					Publicar
+				</a>
+				<Link to="/login">Login</Link>
+				<Link to="/register">Register</Link>
 			</nav>
+			<div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center">
+				<div className="w-4 h-4 bg-gray-500 rounded-full"></div>
+			</div>
 		</header>
 	);
 };
