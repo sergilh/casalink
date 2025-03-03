43c96da7980f0c8c323b16d841b23dd6087f83be
David González <glez_david-93@hotmail.com>
2025-03-03 19:43:35 +0100

    cambios menores en registro alquiler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-<<<<<<< HEAD
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -22,53 +21,5 @@ function App() {
     </>
   );
 }
-=======
-import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import UpdateProductPage from './pages/UpdateProductPage';
-import RentalRequestsPage from './pages/RentalRequestsPage';
-import CreateRentWithImages from './pages/CreateRentWithImages'; // Variante con imágenes
-import ContactPage from './pages/ContactPage';
-import AboutPage from './pages/AboutPage';
-import HelpPage from './pages/HelpPage';
-import './index.css';
-
-const App = () => {
-	return (
-		<>
-			<div className="flex min-h-screen flex-col">
-				<Header />
-
-				<Routes>
-					{/* Rutas Públicas */}
-					<Route path="/" element={<HomePage />} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/register" element={<RegisterPage />} />
-					<Route path="/about" element={<AboutPage />} />
-					<Route path="/contact" element={<ContactPage />} />
-					<Route path="/help" element={<HelpPage />} />
-
-					{/* Rutas de Gestión */}
-					<Route
-						path="/create-rent"
-						element={<CreateRentWithImages />}
-					/>
-					<Route
-						path="/update-product"
-						element={<UpdateProductPage />}
-					/>
-					<Route
-						path="/rental-requests"
-						element={<RentalRequestsPage />}
-					/>
-				</Routes>
-
-				<Footer />
-			</div>
-		</>
-	);
-};
->>>>>>> cf017f873b80826079e5b48962e875ecdf8b3e6e
 
 export default App;
