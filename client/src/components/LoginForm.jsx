357d31bb901af104c56b4dd3dfb0f3c80d519eeb
Ken Russel Claveria <koiclaveria@gmail.com>
2025-03-04 22:14:37 +0100

    actualizado loginPage

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // eslint-disable-next-line react/prop-types
 function Button({ value }) {
 	return (
@@ -5,22 +6,59 @@ function Button({ value }) {
 			// eslint-disable-next-line no-undef
 			onClick={() => preventDefault(e)}
 			className="mt-6 transition transition-all block py-3 px-4 w-full text-white font-bold rounded cursor-pointer bg-[#ff6666] hover:bg-[#66ffff] transform hover:-translate-y-1 hover:shadow-lg"
+=======
+// Componente de Botón reutilizable
+// eslint-disable-next-line react/prop-types
+
+function Button({ value, disabled }) {
+	return (
+		<button
+			type="submit"
+			disabled={disabled}
+			className={`w-full py-2 text-white font-semibold rounded-full transition duration-300 ${
+				disabled
+					? 'bg-gray-400 cursor-not-allowed'
+					: 'bg-[#FF6666] hover:bg-[#E05555]'
+			}`}
+>>>>>>> 39015f5 (actualizado loginPage)
 		>
 			{value}
 		</button>
 	);
 }
 
+<<<<<<< HEAD
 // eslint-disable-next-line react/prop-types
 function Input({ type, id, name, label, placeholder, autofocus }) {
 	return (
 		<label className="text-gray-500 block mt-3">
 			{label}
+=======
+// Componente de Input reutilizable
+// eslint-disable-next-line 'react/prop-types'
+
+function Input({
+	type,
+	id,
+	name,
+	label,
+	placeholder,
+	value,
+	onChange,
+	autofocus,
+}) {
+	return (
+		<div>
+			<label className="block text-sm font-medium text-gray-600">
+				{label}
+			</label>
+>>>>>>> 39015f5 (actualizado loginPage)
 			<input
 				autoFocus={autofocus}
 				type={type}
 				id={id}
 				name={name}
+<<<<<<< HEAD
 				placeholder={placeholder}
 				className="rounded px-4 py-3 w-full mt-1 bg-white text-gray-900 border border-gray-200 focus:border-indigo-400 focus:outline-none focus:ring focus:ring-indigo-100"
 			/>
@@ -34,18 +72,47 @@ function LoginForm() {
 			<div className=" border-t-8 border-[#ff6666] bg-white p-12 shadow-2xl w-96">
 				<h1 className="font-bold text-center block text-2xl">Log In</h1>
 				<form>
+=======
+				value={value}
+				onChange={onChange}
+				placeholder={placeholder}
+				className="w-full px-4 py-2 mt-1 border border-gray-300 bg-white text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500 placeholder:text-gray-500"
+				required
+			/>
+		</div>
+	);
+}
+
+// Formulario de Login
+function LoginForm({ formValues, handleChange, handleLoginUser, loading }) {
+	return (
+		<section className="flex min-h-screen items-center justify-center bg-gray-100">
+			<article className="w-full max-w-md p-6 space-y-6 bg-white shadow-lg rounded-lg">
+				<h2 className="text-2xl font-bold text-center text-gray-700">
+					Iniciar Sesión
+				</h2>
+
+				<form className="space-y-4" onSubmit={handleLoginUser}>
+>>>>>>> 39015f5 (actualizado loginPage)
 					<Input
 						type="email"
 						id="email"
 						name="email"
 						label="Correo Electrónico"
+<<<<<<< HEAD
 						placeholder="me@example.com"
+=======
+						placeholder="ejemplo@email.com"
+						value={formValues.email}
+						onChange={handleChange}
+>>>>>>> 39015f5 (actualizado loginPage)
 						autofocus={true}
 					/>
 					<Input
 						type="password"
 						id="password"
 						name="password"
+<<<<<<< HEAD
 						label="Password"
 						placeholder="••••••••••"
 					/>
@@ -53,6 +120,30 @@ function LoginForm() {
 				</form>
 			</div>
 		</div>
+=======
+						label="Contraseña"
+						placeholder="********"
+						value={formValues.password}
+						onChange={handleChange}
+					/>
+					<Button
+						value={loading ? 'Cargando...' : 'Iniciar Sesión'}
+						disabled={loading}
+					/>
+				</form>
+
+				<p className="text-sm text-center text-gray-500">
+					¿No tienes una cuenta?{' '}
+					<a
+						href="/register"
+						className="text-blue-500 hover:underline"
+					>
+						Regístrate
+					</a>
+				</p>
+			</article>
+		</section>
+>>>>>>> 39015f5 (actualizado loginPage)
 	);
 }
 
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,6 +3,8 @@ import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
 import toast from 'react-hot-toast';
 
+import LoginForm from '../components/LoginForm'; // Importamos el nuevo formulario modularizado
+
 const { VITE_API_URL } = import.meta.env;
 
 const LoginPage = () => {
@@ -49,72 +51,12 @@ const LoginPage = () => {
 	if (authUser) return <Navigate to="/" />;
 
 	return (
-		<section className="flex min-h-screen items-center justify-center bg-gray-100">
-			<article className="w-full max-w-md p-6 space-y-6 bg-white shadow-lg rounded-lg">
-				<h2 className="text-2xl font-bold text-center text-gray-700">
-					Iniciar Sesión
-				</h2>
-
-				<form className="space-y-4" onSubmit={handleLoginUser}>
-					{/* Email */}
-					<div>
-						<label className="block text-sm font-medium text-gray-600">
-							Correo Electrónico
-						</label>
-						<input
-							type="email"
-							name="email"
-							value={formValues.email}
-							onChange={handleChange}
-							className="w-full px-4 py-2 mt-1 border border-gray-300 bg-white text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500 placeholder:text-gray-500"
-							placeholder="ejemplo@email.com"
-							required
-						/>
-					</div>
-
-					{/* Contraseña */}
-					<div>
-						<label className="block text-sm font-medium text-gray-600">
-							Contraseña
-						</label>
-						<input
-							type="password"
-							name="password"
-							value={formValues.password}
-							onChange={handleChange}
-							className="w-full px-4 py-2 mt-1 border border-gray-300 bg-white text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-primary-500 placeholder:text-gray-500"
-							placeholder="********"
-							required
-						/>
-					</div>
-
-					{/* Botón de Login */}
-					{/* Botón de Login */}
-					<button
-						type="submit"
-						className={`w-full py-2 text-white font-semibold rounded-full transition duration-300 ${
-							loading
-								? 'bg-gray-400 cursor-not-allowed'
-								: 'bg-[#FF6666] hover:bg-[#E05555]'
-						}`}
-						disabled={loading}
-					>
-						{loading ? 'Cargando...' : 'Iniciar Sesión'}
-					</button>
-				</form>
-
-				{/* Link de Registro */}
-				<p className="text-sm text-center text-gray-500">
-					¿No tienes una cuenta?{' '}
-					<a
-						href="/register"
-						className="text-blue-500 hover:underline"
-					>
-						Regístrate
-					</a>
-				</p>
-			</article>
-		</section>
+		<LoginForm
+			formValues={formValues}
+			handleChange={handleChange}
+			handleLoginUser={handleLoginUser}
+			loading={loading}
+		/>
 	);
 };
 
