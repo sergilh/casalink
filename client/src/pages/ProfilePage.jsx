d0d9ff09a4b444a1da17d8796da61817eeb2ca64
johnatanmoran <info@johnatanmoran.com>
2025-03-05 17:45:43 +0100

    Rama de resultados de búsqueda

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,82 +1,77 @@
-import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import toast from "react-hot-toast";
-import { AuthContext } from "../contexts/AuthContext";
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../contexts/AuthContext';
 const { VITE_API_URL } = import.meta.env;
 
 const ProfilePage = () => {
-    const { userId } = useParams();
-    const { authUser } = useContext(AuthContext);
-    const token = authUser?.token || localStorage.getItem('token'); // Obtener token
-    const [userReviews, setUserReviews] = useState([]);
-    const [userInfo, setUserInfo] = useState({});
+	const { userId } = useParams();
+	const { authUser } = useContext(AuthContext);
+	const token = authUser?.token || localStorage.getItem('token'); // Obtener token
+	const [userReviews, setUserReviews] = useState([]);
+	const [userInfo, setUserInfo] = useState({});
 
-    const [ loading, setLoading ] = useState(true);
+	const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const getUserReviews = async () => {
-            try {
-                const res = await fetch(`${VITE_API_URL}/api/users/${userId}/reviews`,{
-                    headers: {
-                        Authorization:`Bearer ${token}`,
-                    },
-        
-                }
-            )
-                if (!res.ok) {
-                    throw new Error('Error al obtener los datos del usuario')
-                }
-                const body = await res.json();
-                console.log('datos recibidos', body)
-                setUserReviews(body.data.reviews);
-                setUserInfo(body.data)
-                
-            } catch (error) {
-                console.error(error);
-                toast.error('Error al obtener los datos del usuario')
-            } finally {
-                setLoading(false);
-            }
-        }
-        
-        if (token) {
-            getUserReviews();
-        }
-    }, [userId,token])
-    
-    
-    return (
-        <main>
-            <h2>Mi perfil</h2>
+	useEffect(() => {
+		const getUserReviews = async () => {
+			try {
+				const res = await fetch(
+					`${VITE_API_URL}/api/users/${userId}/reviews`,
+					{
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					}
+				);
+				if (!res.ok) {
+					throw new Error('Error al obtener los datos del usuario');
+				}
+				const body = await res.json();
+				console.log('datos recibidos', body);
+				setUserReviews(body.data.reviews);
+				setUserInfo(body.data);
+			} catch (error) {
+				console.error(error);
+				toast.error('Error al obtener los datos del usuario');
+			} finally {
+				setLoading(false);
+			}
+		};
 
-            <section>
-                <div>
-                    <h2>{userReviews.name}</h2>
-                    <p>{userInfo.averageRating}</p>
-                </div>
-            </section>
+		if (token) {
+			getUserReviews();
+		}
+	}, [userId, token]);
 
-            {loading ? (
-                <p>Cargando...</p>
-            ) : userReviews.length > 0 ? (
-                <div>
-                    {userReviews.map((review)=>(
-                        <div key={review.id}>
-                            <p>Calificación: {review.rating}</p>
-                            <p>{review.reviewerId}</p>
-                            <p>Comentario: {review.comment}</p>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <p>No hay reseñas disponibles para este usuario</p>
-                        
-            )}
-            
-        </main>
+	return (
+		<main>
+			<h2>Mi perfil</h2>
 
-        
-    )
-}
+			<section>
+				<div>
+					<h2>{userReviews.name}</h2>
+					<p>{userInfo.averageRating}</p>
+				</div>
+			</section>
 
-export default ProfilePage
+			{loading ? (
+				<p>Cargando...</p>
+			) : userReviews.length > 0 ? (
+				<div>
+					{userReviews.map((review) => (
+						<div key={review.id}>
+							<p>Calificación: {review.rating}</p>
+							<p>{review.reviewerId}</p>
+							<p>Comentario: {review.comment}</p>
+						</div>
+					))}
+				</div>
+			) : (
+				<p>No hay reseñas disponibles para este usuario</p>
+			)}
+		</main>
+	);
+};
+
+export default ProfilePage;
