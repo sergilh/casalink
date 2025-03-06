b9c95ae6bc6ac5a3342a5274efc62cf387657abc
David González <glez_david-93@hotmail.com>
2025-03-06 12:20:05 +0100

    Ahora el botón de la homepage Publica tu anuncion lleva a create-rent

--- client/src/components/PublishPropertySection.jsx
+++ client/src/components/PublishPropertySection.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import oscarGarciaHabla from '../assets/images/casalink-oscar-garcia-hablando.png';
 import oscarGarciaSelfie from '../assets/images/casalink-oscar-garcia-selfie.png';
 import oscarGarciaAjustes from '../assets/images/casalink-oscar-garcia-ajustes.png';
+
 const PublishPropertySection = () => {
+  const navigate = useNavigate();
+
 	return (
 		<section>
 			{/* Steps Section */}
@@ -35,18 +39,22 @@ const PublishPropertySection = () => {
 						<div className="flex items-center justify-center mb-2">
 							<img
 								src={oscarGarciaHabla}
-								alt="Habla con usuarios y alquila yu propiedad"
+								alt="Habla con usuarios y alquila tu propiedad"
 							/>
 						</div>
 						<span className="text-white text-center text-2xl md:text-base">
 							Alquila con Seguridad
 						</span>
 					</div>
-					<div className="flex flex-col items-center w-full">
-						<button className="bg-[#000033] hover:bg-[#66ffff] transition-all duration-300 w-full text-[#eeeeee] hover:text-[#000033] px-6 py-2 rounded-full transform hover:scale-105 text-3xl md:text-base">
-							¡Publica tu anuncio!
-						</button>
-					</div>
+					
+          <div className="flex flex-col items-center w-full">
+            <button
+              onClick={() => navigate('/create-rent')}
+              className="bg-[#000033] hover:bg-[#66ffff] transition-all duration-300 w-full text-[#eeeeee] hover:text-[#000033] px-6 py-2 rounded-full transform hover:scale-105 text-3xl md:text-base"
+            >
+              ¡Publica tu anuncio!
+            </button>
+          </div>
 				</div>
 			</div>
 		</section>
@@ -54,3 +62,4 @@ const PublishPropertySection = () => {
 };
 
 export default PublishPropertySection;
+
