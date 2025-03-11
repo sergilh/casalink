dcc1af8678ea0ede841d980d7d8bf0ea8d9d0f09
Sergi López Hernández <serkio.lh@gmail.com>
2025-03-11 17:14:01 +0100

    Pagina de Reviews funcionando

--- server/src/models/contracts/selectContractByStatusModel.js
+++ server/src/models/contracts/selectContractByStatusModel.js
@@ -27,7 +27,7 @@ const selectContractsByStatusModel = async ({
 	// Consulta para obtener los contratos paginados
 	const [contracts] = await pool.query(
 		`
-			SELECT c.*, p.propertyTitle, p.propertyType, p.price, p.addressLocality FROM contracts c JOIN properties p ON c.propertyId=p.id WHERE c.tenantId = ? AND c.status IN (?)
+			SELECT c.*, p.propertyTitle, p.ownerId, p.propertyType, p.price, p.addressLocality, u.name AS ownerName FROM contracts c  JOIN properties p ON c.propertyId=p.id JOIN users u ON p.ownerId = u.id WHERE c.tenantId = ? AND c.status IN (?)
 			ORDER BY c.createdAt DESC
 			LIMIT ? OFFSET ?
 			`,
