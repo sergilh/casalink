88f016afb9cb4e381ba3cb68b3c3e416095cb4be
Sergi López Hernández <serkio.lh@gmail.com>
2025-03-11 20:37:30 +0100

    Pagina de Reviews funcionando

--- client/src/pages/PublishReview.jsx
+++ client/src/pages/PublishReview.jsx
@@ -13,6 +13,7 @@ const PublishReview = () => {
   const token = authUser?.token || localStorage.getItem('token'); // Obtener token
   const { userId } = useParams()
   const [formValues, setFormValues] = useState({
+        reviewerRole:'',
         reviewedId: "",
         contractId: "",
         rating: 0,
@@ -27,6 +28,8 @@ const PublishReview = () => {
   
   console.log(formValues);
   console.log(userContracts);
+  console.log(filterProperties);
+  
 
   
     
@@ -48,14 +51,31 @@ const PublishReview = () => {
     }))
   }
 
-  //Función que filtra las propiedades según el nombre que se elija
+  //Función que filtra las propiedades según el nombre  y rol que se elija
   useEffect(() => {
-    if (formValues.reviewedId) {
-      console.log("Filtrando por propietario ID:", formValues.reviewedId);
-      const properties = userContracts.contracts.filter((contract) => contract.ownerId === Number(formValues.reviewedId));
-      setFilterProperties(properties);
+      console.log("------ EJECUTANDO FILTRADO ------");
+  console.log("Rol del reviewer:", formValues.reviewerRole);
+  console.log("ID del usuario autenticado (userId):", userId);
+  console.log("ID del usuario a evaluar (reviewedId):", formValues.reviewedId);
+    console.log("Lista de contratos completos:", userContracts.contracts);
+      if (!formValues.reviewedId) {
+    console.log("❌ reviewedId aún no está definido. No se ejecuta el filtrado.");
+    return; // Salir temprano si aún no hay un reviewedId
+  }
+    if (formValues.reviewerRole && formValues.reviewedId) {
+      let properties = [];
+      if (formValues.reviewerRole === 'tenant') {
+        console.log("Filtrando por propietario ID:", formValues.reviewedId);
+        properties = userContracts.contracts.filter((contract) => contract.tenantId === Number(userId) && contract.ownerId===Number(formValues.reviewedId));
+        setFilterProperties(properties);
+      } else if(formValues.reviewerRole === 'owner'){
+        properties=userContracts.contracts.filter((contract) => contract.ownerId === Number(userId) && contract.tenantId===Number(formValues.reviewedId));
+        setFilterProperties(properties);
+      }else {
+        setFilterProperties([])
+      }
     } 
-  },[formValues.reviewedId,userContracts.contracts])
+  },[formValues.reviewedId,userContracts.contracts,formValues.reviewerRole,userId])
   
   //Función que maneja el envío del formulario
   const sendReview = async (e) => {
@@ -87,10 +107,21 @@ const PublishReview = () => {
          <main className="flex justify-center items-center min-h-screen bg-gray-100">
 			<div className="bg-white shadow-lg rounded-xl p-6 w-full max-w-2xl">
                 <h2 className="text-2xl font-semibold text-gray-700 text-center mb-4">Publicar Reseña</h2>
-                 <form onSubmit={sendReview}  className="grid grid-cols-2 gap-4 w-full">
+          <form onSubmit={sendReview} className="grid grid-cols-2 gap-4 w-full">
+            {/* Selección de rol  */}
+            <div className="col-span-2">
+              <label htmlFor="reviewerRole" className="block text-gray-600 font-medium">Eres:</label>
+              <select name="reviewerRole" value={formValues.reviewerRole} onChange={handleChange} required className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              ><option value="">Selecciona tu rol</option>
+              <option value="tenant">Inquilino</option>
+                <option value="owner">Propietario</option></select>
+              </div>
+                      
+            
+
           {/* Campo ReviewedId  */}
           <div className="col-span-2">
-            <label htmlFor="reviewedId" className="block text-gray-600 font-medium">Nombre del propietario:</label>
+            <label htmlFor="reviewedId" className="block text-gray-600 font-medium">Nombre del propietario/inquilino:</label>
             <select
               type="text"
               name="reviewedId"
@@ -101,8 +132,16 @@ const PublishReview = () => {
               >
                 <option value="">Selecciona un usuario</option>
                 {userContracts.contracts.map((contract) => (
-                  <option key={contract.id} value={contract.ownerId}>{contract.ownerName}</option>
-                ))}  
+                  formValues.reviewerRole === 'tenant' ? (
+                    contract.ownerId !== Number(userId) && (
+                      <option key={contract.id} value={contract.ownerId}>{contract.ownerName}(Propietario)</option>
+                    )
+                  ) : formValues.reviewerRole === 'owner' ? (
+                    contract.tenantId !== Number(userId) && (
+                      <option key={contract.id} value={contract.tenantId}>{contract.tenantName} (Inquilino)</option>
+                    )
+                  ) : null
+                      ))}
               </select>
           </div>
 
