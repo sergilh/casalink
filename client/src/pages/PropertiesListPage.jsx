aff34aba409197ca6164282a9ae6ba37d3ba916c
Ken Russel Claveria <koiclaveria@gmail.com>
2025-03-11 22:53:18 +0100

    tailwineado botones

--- client/src/pages/PropertiesListPage.jsx
+++ client/src/pages/PropertiesListPage.jsx
@@ -195,7 +195,7 @@ const PropertiesListPage = () => {
 										`/properties/${property.propertyId}/update`
 									)
 								}
-								className="mt-3 py-2 px-4 text-white font-bold rounded cursor-pointer bg-[#ff6666] hover:bg-[#E05555]"
+								className="mt-3 py-2 px-4 text-white font-bold rounded-full cursor-pointer bg-[#ff6666] hover:bg-[#E05555]"
 							>
 								Editar Propiedad
 							</button>
