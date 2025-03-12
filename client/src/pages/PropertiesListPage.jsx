b94a484264cb46bd73e3883436c59630bb551903
Ken Russel Claveria <koiclaveria@gmail.com>
2025-03-12 11:24:32 +0100

    actualizado tailwind de la  pagina de no tienes propiedades regristradas

--- client/src/pages/PropertiesListPage.jsx
+++ client/src/pages/PropertiesListPage.jsx
@@ -118,17 +118,19 @@ const PropertiesListPage = () => {
 	// Si el usuario no tiene propiedades, mostramos un mensaje amigable en la UI
 	if (properties.length === 0) {
 		return (
-			<div className="flex flex-col items-center mt-10">
-				<p className="text-gray-500">
-					No tienes propiedades registradas.
-				</p>
-				<button
-					onClick={() => navigate(-1)}
-					className="mt-4 py-2 px-4 text-white font-bold rounded-full bg-[#ff6666] hover:bg-[#E05555]"
-				>
-					Volver atrás
-				</button>
-			</div>
+			<main className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-6">
+				<div className="text-center">
+					<p className="text-gray-500 text-lg">
+						No tienes propiedades registradas.
+					</p>
+					<button
+						onClick={() => navigate(-1)}
+						className="mt-4 py-2 px-4 text-white font-bold rounded-full bg-[#ff6666] hover:bg-[#E05555]"
+					>
+						Volver atrás
+					</button>
+				</div>
+			</main>
 		);
 	}
 
