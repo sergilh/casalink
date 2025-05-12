eea191c5856fceee3a9d6e61d1e5f7cf8310a854
Sergi López Hernández <serkio.lh@gmail.com>
2025-05-12 21:51:39 +0200

    Ajustes .env para deploy

--- server/src/db/getPool.js
+++ server/src/db/getPool.js
@@ -15,6 +15,7 @@ const getPool = async () => {
 				host: MYSQL_HOST,
 				user: MYSQL_USER,
 				password: MYSQL_PASS,
+				port: MYSQL_PORT,
 			});
 
 			// Creamos la base de datos si no existe.
